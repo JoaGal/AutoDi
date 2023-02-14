d8c9b5fe874440e3f6b8629b9f5b8db80b2fbb4b
JoaGal <joaquingaldeano7@gmail.com>
2023-02-14 17:24:34 -0300

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
+import leaves from './assets/leaves.svg';
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1 className="text-3xl font-bold underline">Q11 yoakim</h1>
+      <div className="flex justify-center items-center bg-lime-500 h-72">
+        <h1 className="text-3xl font-bold no-underline text-stone-50 mr-2">AutoDi</h1>
+        <Image src={leaves} alt="leaves" width={30}/>
       </div>
     </main>
   );
