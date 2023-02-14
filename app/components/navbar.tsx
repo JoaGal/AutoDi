669629073dd56d7dbb304e4a9d2ea4dd7addb58b
GiuliannT <giuliconti1@gmail.com>
2023-02-14 16:37:30 -0300

    test

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,17 +1,20 @@
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
-    <nav className="bg-green-400 text-white p-4">
+    <nav className="bg-green-300 text-black p-4 fixed flex items-center justify-center h-12 top-0 left-0 w-full z-100 bg_bottom">
       <ul className="flex space-x-4">
         <li>
-          <a href="/">Home</a>
+          <Link href="/">Inicio</Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link href="/diet">Dieta</Link>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <Link href="/contact">Contacto</Link>
         </li>
       </ul>
+      {/* <img src="bg.svg" alt="bg" className="h-12 ml-4" /> */}
     </nav>
   );
 }
