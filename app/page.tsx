8baf58f295392a2147d30abdf60ce68f38d78578
GiuliannT <giuliconti1@gmail.com>
2023-02-14 15:21:44 -0300

    tailwind added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import styles from './page.module.css'
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>Hola todo bien Giuliant</h1>
+    <main>
+      <div>
+        <h1 className="text-3xl font-bold underline">Q11 yoakim</h1>
       </div>
     </main>
   )
