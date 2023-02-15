499b7bc6d81509ea9e7459ce51c5c6f6fe11a92a
JoaGal <joaquingaldeano7@gmail.com>
2023-02-15 12:11:28 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Link href="/" className='text-3xl md:text-5xl font-bold text-center text-stone-50 bg-lime-500 p-2 pb-3 md:p-6 md:pb-7 shadow-lg rounded-lg drop-shadow-lg animate__animated animate__bounceInDown z-20'>Comenzar</Link>
       </div>
       {show && 
-      <div className='hola z-30 absolute overflow-y-scroll bottom-20 md:bottom-32 md:left-32 left-20  w-48 md:w-96 h-64 md:h-96 p-2 md:p-10 bg-white shadow-lg rounded-lg drop-shadow-lg animate__animated animate__bounceIn'>
+      <div className='hola z-30 absolute overflow-y-scroll bottom-16 md:bottom-32 md:left-32 left-20  w-48 md:w-96 h-64 md:h-96 p-2 md:p-10 bg-stone-50 shadow-lg rounded-bl-none rounded-2xl drop-shadow-lg animate__animated animate__bounceIn'>
         <p className='font-bold text-xs md:text-base'>
           Aqui descubrirás cuántas calorías debes consumir a diario, si tu peso y composición corporal son saludables. Nuestra calculadora nutricional gratis te dirá cuál es tu índice de masa corporal (IMC) y tu porcentaje de grasa corporal.
           Vas a poder tener tu propio plan alimenticio personalizado, con el que podrás perder peso o ganar músculo. Además, te daremos consejos para que puedas llevar una vida saludable.
