9a291239305c319fa19960a5b4e50be8d8260499
JoaGal <joaquingaldeano7@gmail.com>
2023-02-15 10:35:21 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,15 +19,15 @@ export default function Home() {
         <Link href="/" className='text-3xl md:text-5xl font-bold text-center text-stone-50 bg-lime-500 p-2 pb-3 md:p-6 md:pb-7 shadow-lg rounded-lg drop-shadow-lg animate__animated animate__bounceInDown z-20'>Comenzar</Link>
       </div>
       {show && 
-      <div className='hola z-30 absolute bottom-32 overflow-y-scroll left-32 w-96 h-96 p-10 bg-white shadow-lg rounded-lg drop-shadow-lg animate__animated animate__bounceIn'>
-        <p className='font-bold'>
+      <div className='hola z-30 absolute overflow-y-scroll bottom-20 md:bottom-32 md:left-32 left-20  w-48 md:w-96 h-64 md:h-96 p-2 md:p-10 bg-white shadow-lg rounded-lg drop-shadow-lg animate__animated animate__bounceIn'>
+        <p className='font-bold text-xs md:text-base'>
           Aqui descubrirás cuántas calorías debes consumir a diario, si tu peso y composición corporal son saludables. Nuestra calculadora nutricional gratis te dirá cuál es tu índice de masa corporal (IMC) y tu porcentaje de grasa corporal.
           Vas a poder tener tu propio plan alimenticio personalizado, con el que podrás perder peso o ganar músculo. Además, te daremos consejos para que puedas llevar una vida saludable.
           Uno de nuestros objetivos es que tardes lo menos posible en armar y preparar tu dieta. Ya sea de la mano de un nutricionista o por tu cuenta, te ayudaremos a que puedas llevarla a cabo.
           Vas a poder modificar los valores que quieras y ver cómo afectan a tu dieta. También vas a poder guardar tus recetas favoritas y crear tu propia lista de la compra. ¡Todo esto y mucho más gratis en AutoDi!
         </p>
       </div>}
-        <button className='text-1xl md:text-3xl font-bold text-lime-500 font-black cursor-pointer z-30 absolute bottom-20 left-20' onClick={() => setShow(!show)}>Quienes somos?</button>
+        <button className='text-1xl md:text-3xl font-bold text-lime-500 font-black cursor-pointer z-30 absolute md:bottom-20 bottom-10 md:left-20 left-10' onClick={() => setShow(!show)}>Quienes somos?</button>
       <div className='bg-home blur-sm brightness-50'></div>
     </main>
   );
